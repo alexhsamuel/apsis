d22eb5bd940a540d0aeac446da4f759a149ea349
Alex Samuel <alex@alexsamuel.net>
2016-05-03 11:22:50 -0400

    Protect from execute on import.

--- python/sched/test.py
+++ python/sched/test.py
@@ -8,31 +8,31 @@ from   .instance_db import InstanceDB
 from   .job import Job
 from   .scheduler import Scheduler
 
+if __name__ == "__main__":
+    logging.getLogger(None).setLevel(logging.DEBUG)
 
-logging.getLogger(None).setLevel(logging.DEBUG)
+    job1 = Job(1, "/bin/sleep 1")
+    job2 = Job(2, "echo 'job starting' pid=$$; /bin/sleep 2; echo 'job done' pid=$$")
 
-job1 = Job(1, "/bin/sleep 1")
-job2 = Job(2, "echo 'job starting' pid=$$; /bin/sleep 2; echo 'job done' pid=$$")
+    ready_queue = []
+    db = InstanceDB("instance-db.pickle")
 
-ready_queue = []
-db = InstanceDB("instance-db.pickle")
+    def start_job(job):
+        instance = Instance(job, random.randint(0, sys.maxsize))
+        db.create_instance(instance)
+        ready_queue.append(instance)
 
-def start_job(job):
-    instance = Instance(job, random.randint(0, sys.maxsize))
-    db.create_instance(instance)
-    ready_queue.append(instance)
+    sched = Scheduler(ready_queue, db)
 
-sched = Scheduler(ready_queue, db)
+    start_job(job2)
+    start_job(job1)
+    start_job(job2)
 
-start_job(job2)
-start_job(job1)
-start_job(job2)
+    for _ in range(15):
+        sched.run1()
+        time.sleep(0.1)
 
-for _ in range(15):
-    sched.run1()
-    time.sleep(0.1)
+    start_job(job1)
+    start_job(job2)
 
-start_job(job1)
-start_job(job2)
-
-sched.run_all()
+    sched.run_all()
