5bcca263aa241ee5d2a670b100d9314c4cea9fbb
Alex Samuel <alex@alexsamuel.net>
2018-07-07 13:04:54 -0600

    Import CSS and navbar.

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,23 +1,28 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <navbar/>
     <router-view/>
   </div>
 </template>
 
 <script>
+import navbar from './components/navbar.vue'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    navbar,
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  width: 90%;
+  max-width: none;
+  margin-left: auto;
+  margin-right: auto;
+  font-family: "Roboto", "HelveticaNeue", "Helvetica Neue", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
+
