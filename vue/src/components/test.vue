fc3cb7758cb4f3e09a8652718c8a2558d38384ab
Alex Samuel <alex@alexsamuel.net>
2018-07-07 13:04:03 -0600

    Full vue-webpack app skeleton.

---
+++ vue/src/components/test.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>message: {{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  props: ['msg'],
+}
+</script>
+
+<style scoped>
+h1 {
+  color: green;
+}
+</style>
