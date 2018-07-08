265cbf251ac4d097f7f51420159c0a237e0737bc
Alex Samuel <alex@alexsamuel.net>
2018-07-08 11:14:36 -0600

    Add time zone select to clock.

--- vue/src/components/Clock.vue
+++ vue/src/components/Clock.vue
@@ -1,7 +1,12 @@
 <template>
-  <span>
-    {{ formatTime(store.state.time, store.state.timeZone) }}
-  </span>
+  <div>
+    <span>
+      {{ formatTime(store.state.time, store.state.timeZone) }}
+    </span>
+    <select id="timeZone" v-model="timeZone" v-on:change="onTimeZoneChange">
+      <option v-for="tz in timeZones" v-bind:value="tz">{{ shortTimeZone(tz) }}</option>
+    </select>
+  </div>
 </template>
 
 <script>
@@ -19,18 +24,39 @@ export default {
   data() {
     return {
       store,
+      timeZone: store.state.timeZone,
+      timeZones: [
+        Intl.DateTimeFormat().resolvedOptions().timeZone,
+        'UTC',
+        'Asia/Tokyo',
+        'Europe/London',
+        'America/New_York',
+      ],
     }
   },
 
   methods: {
-    formatTime
+    formatTime,
+
+    shortTimeZone(tz) {
+      const parts = tz.split('/')
+      return parts[parts.length - 1].replace('_', ' ')
+    },
+
+    onTimeZoneChange(event) {
+      const tz = event.target.value
+      this.store.setTimeZone(tz)
+    }
   },
 
 }
 </script>
 
 <style scoped>
-span {
+#timeZone {
+  border: 1px solid #e0e0e0;
+  height: 3.5ex;
+  font-weight: 400;
 }
 </style>
 
