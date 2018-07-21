81b04a6ce6993c51259b01f2cf2f540c5ed27635
Alex Samuel <alex@alexsamuel.net>
2018-07-21 08:26:36 -0400

    Use an explicit heap for scheduled runs.

--- python/apsis/scheduled.py
+++ python/apsis/scheduled.py
@@ -1,4 +1,5 @@
 import asyncio
+import heapq
 import logging
 from   ora import now
 
@@ -7,9 +8,9 @@ log = logging.getLogger(__name__)
 #-------------------------------------------------------------------------------
 # Helpers
 
-def at(time, coro):
-    return asyncio.get_event_loop().call_later(
-        time - now(), lambda: asyncio.ensure_future(coro()))
+async def soon(delay, coro):
+    await asyncio.sleep(delay)
+    await coro
 
 
 #-------------------------------------------------------------------------------
@@ -19,31 +20,73 @@ class ScheduledRuns:
     Scheduled runs waiting to execute.
     """
 
+    # We maintain a time-ordered heap of scheduled runs, rather than using
+    # the event loop, to guarantee that we are scheduling to the real time
+    # clock, rather than the event loop's clock.
+    #
+    # The ready loop sleeps until the next scheduled job is ready, or for
+    # MAX_DELAY, whichever comes first.  MAX_DELAY is important becayse event
+    # loop clock may wander from the real time clock.  (This happens
+    # dramatically if the computer sleeps.)  At most, the ready loop sleeps
+    # for MAX_DELAY before rechecking the real time clock.
+
+    MAX_DELAY = 1
+
     def __init__(self, start_run):
+        """
+        :param start_run:
+          Async function that starts a run.
+        """
         self.__start_run    = start_run
-        self.__runs         = {}
+        self.__heap         = []
+
+        # Get the ready loop started.
+        self.__ready_task   = asyncio.ensure_future(self.__ready_loop())
 
 
     def __len__(self):
-        return len(self.__runs)
+        return len(self.__heap)
 
 
-    async def __ready(self, run):
-        # Clean up the task.
-        del self.__runs[run]
-        # Get going.
-        await self.__start_run(run)
+    async def __ready_loop(self):
+        while True:
+            while len(self.__heap) > 0 and self.__heap[0][0] <= now():
+                # The next run is ready.
+                _, run = heapq.heappop(self.__heap)
+                await self.__start_run(run)
+
+            # Sleep until the next run is ready, or MAX_DELAY.
+            await asyncio.sleep(
+                self.MAX_DELAY if len(self.__heap) == 0
+                else min(self.MAX_DELAY, self.__heap[0][0] - now())
+            )
 
 
     def schedule(self, time, run):
-        log.info(f"schedule: {time} {run.run_id}")
-        task = at(time, lambda: self.__ready(run))
-        self.__runs[run] = task
+        wait = time - now()
+        if wait <= 0:
+            # Job is ready; run it now.
+            log.info(f"run immediately: {time} {run.run_id}")
+            asyncio.ensure_future(self.__start_run(run))
+
+        elif wait <= self.MAX_DELAY:
+            # Job is almost ready; use the event loop, sine the ready loop may
+            # be sleeping for a while.  
+            # FIXME: Runs scheduled this way can't be unscheduled.
+            log.info(f"run soon: {time} {run.run_id}")
+            asyncio.ensure_future(soon(wait, self.__start_run(run)))
+
+        else:
+            # Put it onto the schedule heap.
+            log.info(f"schedule: {time} {run.run_id}")
+            heapq.heappush(self.__heap, (time, run))
 
 
     def unschedule(self, run):
         log.info(f"unschedule: {run}")
-        task = self.__runs.pop(run)
-        task.cancel()
+        # FIXME: This is horrible.  Figure out a better way.
+        self.__heap = [ (t, r) for t, r in self.__heap if r is not run ]
+        heapq.heapify(self.__heap)
+
 
 
