2fb5cc8eb210c437740be2b3b6d9126ba9c429a1
Alex Samuel <alex@alexsamuel.net>
2018-07-22 09:37:45 -0400

    WIP

--- vue/src/components/RunsList.vue
+++ vue/src/components/RunsList.vue
@@ -4,13 +4,14 @@
     <table class="uk-table uk-table-divider uk-table-hover uk-table-small uk-table-justify">
       <thead>
         <tr>
-          <th>Job</th>
-          <th>Args</th>
+          <th class="col-job">Job</th>
+          <th class="col-args">Args</th>
           <th class="col-run">Run</th>
+          <th class="col-reruns">Reruns</th>
           <th class="col-state">State</th>
-          <th>Schedule</th>
-          <th>Start</th>
-          <th>Elapsed</th>
+          <th class="col-schedule-time">Schedule</th>
+          <th class="col-start-time">Start</th>
+          <th class="col-elapsed">Elapsed</th>
           <th>Actions</th>
         </tr>
       </thead>
@@ -24,10 +25,31 @@
             </td>
           </tr>
           -->
-          <tr v-for="run in rerun_group" :key="run.run_id">
-            <td><Job v-bind:job-id="run.job_id"></Job></td>
-            <td>{{ arg_str(run.args) }}</td>
+          <tr 
+              v-for="(run, index) in rerun_group" 
+              :key="run.run_id"
+              v-bind:class="{ 'run-group-next': index > 0 }"
+            >
+            <td class="col-job"><Job v-bind:job-id="run.job_id"></Job></td>
+            <td class="col-args"><span>{{ arg_str(run.args) }}</span></td>
             <td class="col-run"><Run v-bind:run-id="run.run_id"></Run></td>
+            <td class="col-reruns">
+              <span>{{ rerun_group.length }}</span>
+              <span v-if="index == 0 && rerun_group.length > 1">
+                <a 
+                    v-if="getGroupCollapse(run.run_id)"
+                    uk-icon="icon: chevron-left"
+                    href="#" 
+                    v-on:click="setGroupCollapse(run.run_id, false)"
+                  ></a>
+                <a 
+                    v-else
+                    uk-icon="icon: chevron-down"
+                    href="#" 
+                    v-on:click="setGroupCollapse(run.run_id, true)"
+                  ></a>
+              </span>
+            </td>
             <td class="col-state">
               <span 
                 v-bind:style="'color: ' + color(run.state)" 
@@ -35,9 +57,9 @@
                 >
               </span>
             </td>
-            <td><Timestamp v-bind:time="run.times.schedule"></Timestamp></td>
-            <td><Timestamp v-bind:time="run.times.running"></Timestamp></td>
-            <td class="rt">{{ run.meta.elapsed === undefined ? "" : formatElapsed(run.meta.elapsed) }}</td>
+            <td class="col-schedule-time"><Timestamp v-bind:time="run.times.schedule"></Timestamp></td>
+            <td class="col-start-time"><Timestamp v-bind:time="run.times.running"></Timestamp></td>
+            <td class="col-elapsed">{{ run.meta.elapsed === undefined ? "" : formatElapsed(run.meta.elapsed) }}</td>
             <td>
               <ActionButton
                 v-for="(url, action) in run.actions" :url="url" :action="action" :key="action">
@@ -83,6 +105,7 @@ export default {
     return { 
       runs_socket: null,
       runs: {},
+      groupCollapse: {},
     } 
   },
 
@@ -121,6 +144,18 @@ export default {
       return join(map(toPairs(args), ([k, v]) => k + '=' + v), ' ')
     },
 
+    getGroupCollapse(runId) {
+      let c = this.groupCollapse[runId]
+      if (c === undefined)
+        c = this.groupCollapse[runId] = true
+      return c
+    },
+
+    setGroupCollapse(runId, collapse) {
+      console.log('setGroupCollapse', runId, collapse)
+      this.groupCollapse[runId] = collapse
+    },
+
     formatElapsed,
   },
 
@@ -143,10 +178,6 @@ table {
   width: 100%;
 }
 
-tbody tr.run:hover {
-  background: #f0fff8;
-}
-
 th,
 td {
   padding: 0.1rem;
@@ -156,17 +187,21 @@ td {
   border: none;
 }
 
-.group td {
-  padding-top: 0.5rem;
-  border-top: 1px solid #e0e0e0;
-}
-
 tbody td {
   max-width: 48rem;
 }
 
 .args {
   font-size: 75%;
+}
+
+tr.run-group-next {
+  border-top: none;
+  
+  .col-job a,
+  .col-args span {
+    visibility: hidden;
+  }
 }
 
 .col-run {
