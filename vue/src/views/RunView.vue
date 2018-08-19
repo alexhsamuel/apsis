c43f2165df7879fbc9b97a3afc5085ac055bf670
Alex Samuel <alex@alexsamuel.net>
2018-08-19 18:04:11 -0400

    Dedent.

--- vue/src/views/RunView.vue
+++ vue/src/views/RunView.vue
@@ -1,64 +1,64 @@
 <template lang="pug">
-  div(v-if="run")
+div(v-if="run")
+  div
+    span.title {{ run_id }}
+    //- FIXME: Use navbar or similar to organize.
+    span
+      ActionButton(
+          v-for="(url, action) in run.actions" 
+          :key="action"
+          :url="url" 
+          :action="action" 
+          :button="true"
+        )
+  div
     div
-      span.title {{ run_id }}
-      //- FIXME: Use navbar or similar to organize.
-      span
-        ActionButton(
-            v-for="(url, action) in run.actions" 
-            :key="action"
-            :url="url" 
-            :action="action" 
-            :button="true"
-          )
-    div
-      div
-        Job(v-bind:job-id="run.job_id")
-        |
-        | {{ arg_str }}
+      Job(v-bind:job-id="run.job_id")
+      |
+      | {{ arg_str }}
 
-      table.fields
-        tbody
-          tr
-            th state
-            td: State(:state="run.state" name)
+    table.fields
+      tbody
+        tr
+          th state
+          td: State(:state="run.state" name)
 
-          tr(v-if="run.message")
-            th message
-            td {{ run.message }}
+        tr(v-if="run.message")
+          th message
+          td {{ run.message }}
 
-          tr(v-if="run.program")
-            th program
-            td.no-padding: Program(:program="run.program")
+        tr(v-if="run.program")
+          th program
+          td.no-padding: Program(:program="run.program")
 
-          tr(v-if="run.rerun != run.run_id")
-            th rerun of
-            td: Run(:run-id="run.rerun")
+        tr(v-if="run.rerun != run.run_id")
+          th rerun of
+          td: Run(:run-id="run.rerun")
 
-          tr
-            th times
-            td.no-padding: table.fields: tbody
-              tr(v-for="[name, time] in run_times" :key="name")
-                th {{ name }}
-                td: Timestamp(:time="time")
+        tr
+          th times
+          td.no-padding: table.fields: tbody
+            tr(v-for="[name, time] in run_times" :key="name")
+              th {{ name }}
+              td: Timestamp(:time="time")
 
-          tr(
-            v-on:click="metadataCollapsed = !metadataCollapsed"
-          )
-            th: div(style="white-space: nowrap;")
-              | metadata 
-            td(v-if="metadataCollapsed")
-              div(uk-icon="chevron-right")
-            td.no-padding(v-else)
-              table.fields: tbody
-                tr(v-for="(value, key) in run.meta" :key="key")
-                  th {{ key }}
-                  td(v-html="format(key, value)")
+        tr(
+          v-on:click="metadataCollapsed = !metadataCollapsed"
+        )
+          th: div(style="white-space: nowrap;")
+            | metadata 
+          td(v-if="metadataCollapsed")
+            div(uk-icon="chevron-right")
+          td.no-padding(v-else)
+            table.fields: tbody
+              tr(v-for="(value, key) in run.meta" :key="key")
+                th {{ key }}
+                td(v-html="format(key, value)")
 
-      h5 output
-      a(v-if="run !== null && run.output_len !== null && output === null" v-on:click="load_output()")
-        | (load {{ run.output_len }} bytes)
-      pre.output(v-if="output !== null") {{ output }}
+    h5 output
+    a(v-if="run !== null && run.output_len !== null && output === null" v-on:click="load_output()")
+      | (load {{ run.output_len }} bytes)
+    pre.output(v-if="output !== null") {{ output }}
 </template>
 
 <script>
