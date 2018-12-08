c3d526e47a1fc597f02f8afa3c13633a2c262eb0
Alex Samuel <alex@alexsamuel.net>
2018-12-08 12:59:32 -0500

    Restore old formatter for ws handler.

--- python/apsis/service/main.py
+++ python/apsis/service/main.py
@@ -75,7 +75,10 @@ class QueueHandler(logging.Handler):
                 pass
 
 
-WS_HANDLER = QueueHandler(10000, LOG_FORMATTER)
+WS_HANDLER = QueueHandler(10000, logging.Formatter(
+    fmt="%(asctime)s.%(msecs)03d %(name)-20s [%(levelname)-7s] %(message)s",
+    datefmt="%H:%M:%S",
+))
 
 #-------------------------------------------------------------------------------
 
