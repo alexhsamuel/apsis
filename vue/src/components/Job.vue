4cf104ed9fe50ac23ce9eb151074c70d3d88b440
Alex Samuel <alex@alexsamuel.net>
2018-12-15 11:27:43 -0500

    Use router-link.
    resolves #31

--- vue/src/components/Job.vue
+++ vue/src/components/Job.vue
@@ -1,24 +1,20 @@
 <template lang="pug">
-a.job-link(v-on:click="go()") {{ jobId }}
+router-link(
+  :to="{ name: 'job', params: { job_id: jobId }}"
+) {{ jobId }}
 
 </template>
 
 <script>
 export default {
   props: ['jobId'],
-
-  methods: {
-    go() {
-      this.$router.push({ name: 'job', params: { job_id: this.jobId } })
-    }
-  }
 }
 </script>
 
 <style lang="scss" scoped>
 @import '../styles/vars.scss';
 
-.job-link {
+a {
   color: $apsis-job-color;
   cursor: default;
   white-space: nowrap;
