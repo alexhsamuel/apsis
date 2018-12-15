6c0f4ea919d5874cfae6b3f138f06562ea0a7e15
Alex Samuel <alex@alexsamuel.net>
2018-12-15 11:32:55 -0500

    Use router-link.  Don't use uk-badge.

--- vue/src/components/Run.vue
+++ vue/src/components/Run.vue
@@ -1,33 +1,35 @@
 <template lang="pug">
-span.uk-badge.run-badge(v-on:click="go()") {{ runId }}
+router-link(
+  :to="{ name: 'run', params: { run_id: runId } }"
+) {{ runId }}
 
 </template>
 
 <script>
 export default {
   props: ['runId'],
-
-  methods: {
-    go() {
-      this.$router.push({ name: 'run', params: { run_id: this.runId } })
-    }
-  }
 }
 </script>
 
 <style lang="scss" scoped>
 @import '../styles/vars.scss';
 
-.run-badge {
+a {
   background-color: white;
   color: $apsis-run-color;
+  box-sizing: border-box;
   border: 1px solid $apsis-run-color;
+  border-radius: 500px;
   padding: 0 0.4rem;
+  font-size: 0.875rem;
+  align-items: center;
+  justify-content: center;
   cursor: default;
 
   &:hover {
     color: white;
     background-color: $apsis-run-color;
+    text-decoration: none;
   }
 }
 
