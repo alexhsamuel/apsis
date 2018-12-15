40e446c9cff06219c44481c6e2b743a821b20a0f
Alex Samuel <alex@alexsamuel.net>
2018-12-15 12:59:17 -0500

    Tidy up.

--- python/apsis/lib/async.py
+++ python/apsis/lib/async.py
@@ -3,13 +3,18 @@ import asyncio
 #-------------------------------------------------------------------------------
 
 async def cancel_task(task, name, log):
+    """
+    Cancels and cleans up `task`, with logging as `name`.
+    """
     log.info(f"canceling {name} task")
     if task.cancelled():
         log.info(f"{name} task already canceled")
     else:
         task.cancel()
+
     try:
-        await task
+        return await task
     except asyncio.CancelledError:
         log.info(f"{name} task canceled")
+
 
