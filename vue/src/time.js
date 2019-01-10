7ac48936db4a1243804f3c1f27ba7ef57efe0d75
Alex Samuel <alex@alexsamuel.net>
2019-01-10 09:32:37 -0500

    Simplify elapsed.

--- vue/src/time.js
+++ vue/src/time.js
@@ -9,7 +9,7 @@ export function formatTime(time, tz, format) {
     return ''
 }
 
-export function formatElapsed(elapsed) {
+export function formatDuration(elapsed) {
   return (
       elapsed < 1e-5 ? (elapsed * 1e6).toPrecision(2) + ' µs'
     : elapsed < 1e-3 ? (elapsed * 1e6).toPrecision(3) + ' µs'
@@ -17,6 +17,18 @@ export function formatElapsed(elapsed) {
     : elapsed < 1e+0 ? (elapsed * 1e3).toPrecision(3) + ' ms'
     : elapsed < 1e+1 ? (elapsed      ).toPrecision(2) + ' s'
     : elapsed < 60   ? (elapsed      ).toPrecision(3) + ' s'
+    : elapsed < 3600 ? 
+          Math.trunc(elapsed / 60) 
+        + ':' + ('' + Math.trunc(elapsed % 60)).padStart(2, '0')
+    :     Math.trunc(elapsed / 3660) 
+        + ':' + ('' + Math.trunc(elapsed / 60 % 60)).padStart(2, '0')
+        + ':' + ('' + Math.trunc(elapsed % 60)).padStart(2, '0')
+  )
+}
+
+export function formatElapsed(elapsed) {
+  return (
+      elapsed < 60   ? Math.trunc(elapsed)
     : elapsed < 3600 ? 
           Math.trunc(elapsed / 60) 
         + ':' + ('' + Math.trunc(elapsed % 60)).padStart(2, '0')
