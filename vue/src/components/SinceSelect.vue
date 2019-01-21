cacc4adb7afd4112cf0ea640b681ee8ccf53ffc4
Alex Samuel <alex@alexsamuel.net>
2019-01-21 17:59:49 -0500

    Combo for run since filter.

---
+++ vue/src/components/SinceSelect.vue
@@ -0,0 +1,50 @@
+<template lang="pug">
+span.since-select
+  button.uk-button.uk-button-default(type="button") {{ formatSince(value) }}
+
+  div(uk-dropdown="pos: bottom-left")
+    ul.uk-nav.uk-dropdown-nav
+      li(v-for="since in ['1d', '7d', '30d', '']")
+        a(href="#" v-on:click="$emit('input', since)") {{ formatSince(since) }}
+
+</template>
+
+<script>
+export default {
+  props: ['value'],
+
+  components: {
+  },
+
+  computed: {
+  },
+
+  methods: {
+    formatSince(since) {
+      if (since === '')
+        return 'All'
+
+      const match = since.match(/^(\d+)d$/)
+      if (match) {
+        const days = parseInt(match[1])
+        return days === 1 ? 'Last day' : 'Last ' + days + ' days'
+      }
+
+      return 'Since ' + since
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.since-select {
+  button {
+    width: 100%;
+    padding: 0 8px 0 12px;
+  }
+  li {
+    text-transform: uppercase;
+    margin-right: 3em;
+  }
+}
+</style>
