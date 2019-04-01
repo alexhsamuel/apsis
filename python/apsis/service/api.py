4eee8f15cd6219a387e159acef648c3475862c1b
Alex Samuel <alex@alexsamuel.net>
2019-04-01 09:35:01 -0400

    Bigger chunk.

--- python/apsis/service/api.py
+++ python/apsis/service/api.py
@@ -16,7 +16,7 @@ from   ..waiter import preco_to_jso
 log = logging.getLogger(__name__)
 
 # Max number of runs to send in one websocket message.
-WS_RUN_CHUNK = 128
+WS_RUN_CHUNK = 1024
 WS_RUN_CHUNK_SLEEP = 0.01
 
 #-------------------------------------------------------------------------------
