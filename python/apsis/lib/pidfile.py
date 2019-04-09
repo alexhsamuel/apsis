7d06b044d0658fa1c47e716e0bb0be6a6888e571
Alex Samuel <alex@alexsamuel.net>
2019-04-09 15:53:10 +0100

    Extra digit for pid.

--- python/apsis/lib/pidfile.py
+++ python/apsis/lib/pidfile.py
@@ -31,7 +31,7 @@ class PidFile:
         """
         assert self.file is None
 
-        pid_str = format(os.getpid(), "5d") + "\n"
+        pid_str = format(os.getpid(), "6d") + "\n"
 
         # Open for append.  The file may exist, or not.
         self.file = open(self.path, "a+")
@@ -46,7 +46,7 @@ class PidFile:
                 # But first give it a moment, to reduces races.  FIXME: Yak.
                 time.sleep(0.001)
                 self.file.seek(0)
-                pid_str = self.file.read(6)
+                pid_str = self.file.read(7)
                 pid = int(pid_str.strip())
                 return pid
 
