52f9f5227aebf5f5664e686c5cd22a4cccc623ca
Alex Samuel <alex@alexsamuel.net>
2019-11-16 17:48:35 -0500

    to_signal().

--- python/apsis/lib/sys.py
+++ python/apsis/lib/sys.py
@@ -1,5 +1,7 @@
+from   contextlib import suppress
 import os
 import pwd
+import signal
 
 #-------------------------------------------------------------------------------
 
@@ -10,3 +12,27 @@ def get_username():
     return pwd.getpwuid(os.getuid()).pw_name
 
 
+def to_signal(sig):
+    """
+    Parses a signal number or named signal.
+
+      >>> to_signal(15)
+      <Signals.SIGTERM: 15>
+      >>> to_signal("15")
+      <Signals.SIGTERM: 15>
+      >>> to_signal("SIGTERM")
+      <Signals.SIGTERM: 15>
+      >>> to_signal("TERM")
+      <Signals.SIGTERM: 15>
+
+    """
+    with suppress(TypeError, ValueError):
+        return signal.Signals(int(sig))
+
+    with suppress(KeyError):
+        return signal.Signals[str(sig)]
+
+    with suppress(KeyError):
+        return signal.Signals["SIG" + str(sig)]
+
+    raise ValueError(f"not a signal: {sig}")
