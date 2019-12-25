982919970e124cf1947028f003740e2d26afeeef
Alex Samuel <alex@alexsamuel.net>
2019-12-25 11:58:41 -0500

    Tolerate run with missing program.

--- python/apsis/service/api.py
+++ python/apsis/service/api.py
@@ -126,7 +126,7 @@ def run_to_jso(app, run, summary=False):
             "conds":
                 None if run.conds is None 
                 else [ c.to_jso() for c in run.conds ],
-            "program": run.program.to_jso(),
+            "program": None if run.program is None else run.program.to_jso(),
             # FIXME: Rename to metadata.
             "meta": run.meta,
         })
