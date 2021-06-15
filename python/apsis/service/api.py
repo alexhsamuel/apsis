58b6179160991a0d6c0eabcdacd110bc617a3063
Alex Samuel <alex@alexsamuel.net>
2021-06-15 09:08:35 -0400

    Don't include conditions in run JSO.

--- python/apsis/service/api.py
+++ python/apsis/service/api.py
@@ -115,9 +115,6 @@ def run_to_jso(app, run, summary=False):
 
     if not summary:
         jso.update({
-            "conds":
-                [] if run.conds is None 
-                else [ _to_jso(c) for c in run.conds ],
             "program": None if run.program is None else run.program.to_jso(),
             # FIXME: Rename to metadata.
             "meta": run.meta,
