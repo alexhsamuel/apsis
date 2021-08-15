ac79566869578306e955f317b196e8b38e456383
Alex Samuel <alex@alexsamuel.net>
2021-08-15 09:14:15 -0700

    Bump version: 0.7.0 → 0.9.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with (Path(sys.argv[0]).parent / "README.md").open("rt") as file:
 
 setuptools.setup(
     name            ="apsis",
-    version         ="0.7.0",
+    version         ="0.9.0",
     description     ="Easy-to-use task scheduler",
     long_description=long_description,
     url             ="https://github.com/alexhsamuel/apsis",
