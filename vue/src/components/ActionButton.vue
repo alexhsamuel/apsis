681ede70d0fb8060ef575e86c7779159c24986dd
Alex Samuel <alex@alexsamuel.net>
2021-09-21 23:20:07 -0400

    Confirmation modal for actions.  #130

--- vue/src/components/ActionButton.vue
+++ vue/src/components/ActionButton.vue
@@ -7,22 +7,31 @@
 </template>
 
 <script>
+import { capitalize } from 'lodash'
+import UIkit from 'uikit'
+
+function titleCapitalize(string) {
+  return string.split(' ').map(capitalize).join(' ')
+}
+
 export default {
   name: 'ActionButton',
   props: ['action', 'url', 'button'],
 
   methods: {
     do_action(url) {
-      fetch(url, { method: 'POST' })
-        .then(async (response) => {
-          if (response.ok) {
-            const result = await response.json()
-            if (result.show_run_id)
-              // Got a hint to nav to a new run.
-              // FIXME: Encapsulate this part of the API somewhere.
-              this.$router.push({ name: 'run', params: { run_id: result.show_run_id } })
-          }
-        })
+      UIkit.modal.confirm(titleCapitalize(this.action) + '?').then(() =>
+        fetch(url, { method: 'POST' })
+          .then(async (response) => {
+            if (response.ok) {
+              const result = await response.json()
+              if (result.show_run_id)
+                // Got a hint to nav to a new run.
+                // FIXME: Encapsulate this part of the API somewhere.
+                this.$router.push({ name: 'run', params: { run_id: result.show_run_id } })
+            }
+          })
+      )
     },
 
   },
