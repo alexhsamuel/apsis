60329854dc9e71a2b60da6b9b06dca6272b66ec3
Alex Samuel <alex@alexsamuel.net>
2022-09-28 11:15:30 -0400

    Construct API URLs in js.

---
+++ vue/test/unit/specs/api.spec.js
@@ -0,0 +1,25 @@
+import * as api from '@/api'
+
+describe('Urls', () => {
+  test('mark URL matches', () => {
+    expect(api.getMarkUrl({run_id: 'r1234'}, 'success')).toEqual('/api/v1/runs/r1234/mark/success')
+    expect(api.getMarkUrl({run_id: 'r567'}, 'failure')).toEqual('/api/v1/runs/r567/mark/failure')
+  })
+
+  test('output data URL matches', () => {
+    expect(api.getOutputDataUrl({run_id: 'r1234'}, 'output')).toEqual('/api/v1/runs/r1234/output/output')
+    expect(api.getOutputDataUrl({run_id: 'r1234'}, 'log')).toEqual('/api/v1/runs/r1234/output/log')
+  })
+
+  test('output URL matches', () => {
+    expect(api.getOutputUrl({run_id: 'r1234'})).toEqual('/api/v1/runs/r1234/outputs')
+  })
+
+  test('rerun URL matches', () => {
+    expect(api.getRerunUrl({run_id: 'r1234'})).toEqual('/api/v1/runs/r1234/rerun')
+  })
+
+  test('run URL matches', () => {
+    expect(api.getRunUrl('r1234')).toEqual('/api/v1/runs/r1234')
+  })
+})
