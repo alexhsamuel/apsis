5918efc0648943b2deabc9b4c5f66734b84e37b8
Alex Samuel <alex@alexsamuel.net>
2022-10-03 17:54:00 -0400

    Get rid of uk-input.

--- vue/src/components/SearchInput.vue
+++ vue/src/components/SearchInput.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
-span.uk-inline
-  span.icon.uk-form-icon
+span.inline
+  span.icon
     SearchIcon(color="#999")
-  input.uk-input(
+  input(
     v-model="input"
     v-on:change="search()"
     v-on:keyup.esc="input = ''; search()"
@@ -41,13 +41,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.uk-input {
-  &:focus {
-    border-color: #6b9;  // FIXME: Elsewhere.
-  }
+.inline {
+  display: inline-block;
+  position: relative;
+  max-width: 100%;
 }
 
 .icon {
   pointer-events: auto;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  width: 40px;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+}
+
+input {
+  padding-left: 40px;
 }
 </style>
