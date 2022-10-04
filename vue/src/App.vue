4cd31d4c6b1c22ecc1e2b822f3fad6c7900999ab
Alex Samuel <alex@alexsamuel.net>
2022-10-04 16:09:01 -0400

    Factor out run state update.

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -11,6 +11,7 @@ import navbar from '@/components/navbar'
 import LiveLog from '@/LiveLog.js'
 import RunsSocket from '@/RunsSocket'
 import store from '@/store.js'
+import { updateRuns } from '@/runs.js'
 
 export default {
   name: 'App',
@@ -29,29 +30,8 @@ export default {
 
   created() {
     this.liveLog = new LiveLog(this.store.state.logLines, 1000)
-    this.runsSocket = new RunsSocket((msg) => {
-      const runs = Object.assign({}, this.store.state.runs)
-      let nadd = 0
-      let nchg = 0
-      let ndel = 0
-      for (const runId in msg.runs) {
-        const run = msg.runs[runId]
-        if (!run.state) {
-          delete runs[runId]
-          ndel++
-        }
-        else {
-          if (runId in runs)
-            nchg++
-          else
-            nadd++
-          // We never change the runs, so freeze them to avoid reactivity.
-          runs[runId] = Object.freeze(msg.runs[runId])
-        }
-      }
-      console.log('runs message:', nadd, 'add,', nchg, 'chg,', ndel, 'del')
-      this.store.state.runs = runs
-    })
+    const store = this.store
+    this.runsSocket = new RunsSocket(msg => updateRuns(msg, store.state))
   },
 
   destroyed() {
