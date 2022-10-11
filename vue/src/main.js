93d37a64a2152c1496b35b28bba342bc94863bd8
Alex Samuel <alex@alexsamuel.net>
2022-10-10 23:06:18 -0400

    Remove UIkit js.

--- vue/src/main.js
+++ vue/src/main.js
@@ -3,10 +3,7 @@ import './styles/index.scss'
 import App from './App'
 import router from './router'
 import store from './store'
-import UIkit from 'uikit'
 import Vue from 'vue'
-
-window.UIkit = UIkit
 
 Vue.config.productionTip = false
 
