0a001b1482d0cbf713b2e34ef0c7aa48796ccccc
Alex Samuel <alex@alexsamuel.net>
2022-10-12 11:12:05 -0400

    Positioning.

--- vue/src/components/icons/TriangleIcon.vue
+++ vue/src/components/icons/TriangleIcon.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-   svg(viewBox="0 0 1800 1800", xmlns="http://www.w3.org/2000/svg" width="18px")
+   svg(viewBox="0 0 1800 1800", xmlns="http://www.w3.org/2000/svg" width="18px" style="position: relative; top: 4px")
       path(
         d="M 400 700 L 1400 700 L 900 1300 L 400 700"
         stroke="none"
