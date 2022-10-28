2ec99de8169e5497c5be13ba7cd0e06066c9cf72
Alex Samuel <alex@alexsamuel.net>
2022-10-28 09:36:21 -0700

    Unit test for max_running condition.

--- test/int/test_transitions.py
+++ test/int/test_transitions.py
@@ -6,6 +6,7 @@ via the HTTP client.
 from   contextlib import closing
 import ora
 import pytest
+import time
 
 from   instance import ApsisInstance
 
@@ -60,5 +61,56 @@ def test_skip(inst):
     # It should be skipped.
     run = client.get_run(run_id)
     assert run["state"] == "skipped"
+
+
+def test_max_running(inst):
+    client = inst.client
+
+    JOB = {
+        "program": {
+            "type": "no-op",
+            "duration": "1",
+        },
+        "condition": {
+            "type": "max_running",
+            "count": 1,
+        },
+    }
+
+    # Create and schedule a run.
+    res = client.schedule_adhoc("now", JOB)
+    job_id = res["job_id"]
+    run_id0 = res["run_id"]
+
+    job = client.get_job(job_id)
+    print(job)
+
+    # Schedule a second run with the same (adhoc) job.
+    res = client.schedule(job_id, {}, "now")
+    run_id1 = res["run_id"]
+
+    # The first job should be running.
+    run = client.get_run(run_id0)
+    assert run["state"] == "running"
+
+    # The second job should be waiting.
+    run = client.get_run(run_id1)
+    assert run["state"] == "waiting"
+
+    # Wait and check again.
+    time.sleep(1)
+
+    # The first run should have completed and the second started.
+    run = client.get_run(run_id0)
+    assert run["state"] == "success"
+    run = client.get_run(run_id1)
+    assert run["state"] == "running"
+
+    # Wait and check again.
+    time.sleep(2)
+
+    # The second run should have completed.
+    run = client.get_run(run_id1)
+    assert run["state"] == "success"
 
 
