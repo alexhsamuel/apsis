3ce1f30cbc76b424ebd16c9ab6d6ef647c0fbde5
Alex Samuel <alex@alexsamuel.net>
2022-11-01 10:42:22 -0700

    Basic SkipDuplicate integration test.

--- test/int/test_conditions.py
+++ test/int/test_conditions.py
@@ -31,8 +31,6 @@ def test_args(client):
     """
     Tests that args in dependencies are processed correctly.
     """
-    res = client.get_jobs()
-
     res = client.schedule("dependent", {"date": "2022-11-01", "color": "red"})
     run_id = res["run_id"]
 
@@ -71,6 +69,50 @@ def test_args(client):
 
     # One dependency satisfied, but not the other one.
     res = client.get_run(run_id)
+    assert res["state"] == "running"
+
+
+def test_skip_duplicate(client):
+    """
+    Tests that a run is skipped if an identical run is running.
+    """
+    # Start a run, which runs for 1 s.
+    res = client.schedule("skippable", {"date": "2022-11-01"})
+    run_id = res["run_id"]
+
+    res = client.get_run(run_id)
+    assert res["state"] == "running"
+
+    # Start a couple of duplicates.
+    res = client.schedule("skippable", {"date": "2022-11-01"})
+    run_id0 = res["run_id"]
+    res = client.schedule("skippable", {"date": "2022-11-01"})
+    run_id1 = res["run_id"]
+
+    # They both should have been skipped.
+    res = client.get_run(run_id0)
+    assert res["state"] == "skipped"
+    res = client.get_run(run_id1)
+    assert res["state"] == "skipped"
+
+    # Start a job with a different date.
+    res = client.schedule("skippable", {"date": "2022-12-25"})
+    run_id2 = res["run_id"]
+
+    # This should not have been skipped; its date is different.
+    res = client.get_run(run_id2)
+    assert res["state"] == "running"
+
+    # Wait for the original run to complete.
+    time.sleep(1)
+    res = client.get_run(run_id)
+    assert res["state"] == "success"
+
+    # Now a rerun should be fine.
+    res = client.schedule("skippable", {"date": "2022-11-01"})
+    run_id3 = res["run_id"]
+
+    res = client.get_run(run_id3)
     assert res["state"] == "running"
 
 
