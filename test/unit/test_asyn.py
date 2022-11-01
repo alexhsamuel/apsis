76d6ca94722052963b426d526c1b38a027bbc857
Alex Samuel <alex@alexsamuel.net>
2022-11-01 08:37:39 -0700

    Cleanup async stuff.

--- test/unit/test_asyn.py
+++ test/unit/test_asyn.py
@@ -10,15 +10,9 @@ import apsis.lib.asyn
 async def test_communicate_notimeout():
     argv = ["/bin/bash", "-c", "echo foo; echo bar >&2; sleep 0.1"]
 
-    async def no_timeout():
-        """
-        Non-timeout case.
-        """
-        proc = await asyncio.create_subprocess_exec(
-            *argv, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        return await apsis.lib.asyn.communicate(proc, 0.5)
-
-    out, err = await no_timeout()
+    proc = await asyncio.create_subprocess_exec(
+        *argv, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = await apsis.lib.asyn.communicate(proc, 0.5)
     assert out == b"foo\n"
     assert err == b"bar\n"
 
@@ -27,16 +21,10 @@ async def test_communicate_notimeout():
 async def test_communicate_timeout():
     argv = ["/bin/bash", "-c", "echo foo; echo bar >&2; sleep 0.25"]
 
-    async def timeout():
-        """
-        Timeout case.
-        """
-        proc = await asyncio.create_subprocess_exec(
-            *argv, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        return await apsis.lib.asyn.communicate(proc, 0.1)
-
+    proc = await asyncio.create_subprocess_exec(
+        *argv, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     try:
-        await timeout()
+        await apsis.lib.asyn.communicate(proc, 0.1)
     except asyncio.TimeoutError as exc:
         assert exc.stdout == b"foo\n"
         assert exc.stderr == b"bar\n"
