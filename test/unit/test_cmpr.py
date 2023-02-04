3f4ecf6e45c7f9835a5e29102859c6ca1cd409c7
Alex Samuel <alex@alexsamuel.net>
2023-02-03 22:21:07 -0500

    Test that async compression allows concurrency.

--- test/unit/test_cmpr.py
+++ test/unit/test_cmpr.py
@@ -1,3 +1,4 @@
+import asyncio
 import brotli
 import pytest
 
@@ -15,5 +16,31 @@ async def test_compress_async():
     compressed = await compress_async(data, "br")
     assert len(compressed) < len(data)
     assert brotli.decompress(compressed) == data
+
+
+@pytest.mark.asyncio
+async def test_compress_async_timing():
+    """
+    Tests that compress_async() allows other coros to run concurrently.
+    """
+    data = b"alkjsdhtlkqjhwetrnabsdcvlkjhqaweljkh" * 1048576
+
+    check = 0
+    async def background():
+        nonlocal check
+        for i in range(1000):
+            await asyncio.sleep(0.0001)
+            check += i
+        return check
+
+    # Start the background task.
+    task = asyncio.create_task(background())
+    # It hasn't been scheduled yet.
+    assert not check
+
+    _ = await compress_async(data, "br")
+    # background() should have run concurrently.
+    assert check > 0
+    assert await task == 499500
 
 
