204fbadbdef2a6affac41cfbdccdb2be8a8dc8de
Alex Samuel <alex@alexsamuel.net>
2023-04-20 15:42:09 -0700

    Integration test for StatsProgram.

---
+++ test/int/test_stats.py
@@ -0,0 +1,48 @@
+from   contextlib import closing
+import json
+import ora
+import time
+
+from   instance import ApsisInstance
+
+#-------------------------------------------------------------------------------
+
+def test_stats(tmp_path):
+    path = tmp_path / "stats.json"
+
+    with closing(ApsisInstance()) as inst:
+        inst.create_db()
+        inst.write_cfg()
+        inst.start_serve()
+        inst.wait_for_serve()
+
+        client = inst.client
+
+        res = client.schedule_adhoc("now", {
+            "program": {
+                "type": "apsis.program.internal.stats.StatsProgram",
+                "path": str(path),
+            },
+        })
+        time.sleep(0.2)
+        assert client.get_run(res["run_id"])["state"] == "success"
+
+        res = client.schedule_adhoc("now", {
+            "program": {
+                "type": "apsis.program.internal.stats.StatsProgram",
+                "path": str(path),
+            },
+        })
+        time.sleep(0.2)
+        assert client.get_run(res["run_id"])["state"] == "success"
+
+        with open(path) as file:
+            stats0 = json.loads(next(file))
+            stats1 = json.loads(next(file))
+        assert round(ora.Time(stats1["time"]) - ora.Time(stats0["time"]), 1) == 0.2
+        assert int(stats0["num_running_tasks"]) == 1
+        assert int(stats0["num_scheduled_entries"]) == 0
+        assert int(stats1["num_running_tasks"]) == 1
+        assert int(stats1["num_scheduled_entries"]) == 0
+
+
