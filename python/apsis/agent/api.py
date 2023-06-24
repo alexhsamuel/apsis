50abb4ea68e0c00b842ad86c5489e23bdb332405
Alex Samuel <alex@alexsamuel.net>
2023-06-24 14:21:13 -0400

    Return stack trace in agent 500 response.

--- python/apsis/agent/api.py
+++ python/apsis/agent/api.py
@@ -27,14 +27,19 @@ def response(jso, status=200):
     )
 
 
-def error(msg, status):
-    return response({"error": str(msg)}, status=status)
+def error(msg, status, *, exc_fmt=None):
+    jso = {"error": str(msg)}
+    if exc_fmt is not None:
+        jso["exception"] = exc_fmt
+    log.error(ujson.dumps(jso, indent=2))
+    return response(jso, status=status)
 
 
 def exc_error(exc, status, log=None):
+    exc_fmt = traceback.format_exc().rstrip()
     if log is not None:
-        log(traceback.format_exc().rstrip())
-    return error(str(exc), status)
+        log(exc_fmt)
+    return error(str(exc), status, exc_fmt=exc_fmt)
 
 
 def rusage_to_jso(rusage):
@@ -211,6 +216,7 @@ async def process_signal(req, proc_id, signal):
         signum = int(to_signal(signal))
     except ValueError:
         return error(f"invalid signal: {signal}", 400)
+    1 / 0
     req.app.ctx.processes.kill(proc_id, signum)
     return response({})
 
