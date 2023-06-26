946dd152fe0b239ac42be5098bcefa4dd7af5e1e
Alex Samuel <alex@alexsamuel.net>
2023-06-26 09:52:54 -0400

    Add test for populating the run store.

--- test/unit/test_run_store.py
+++ test/unit/test_run_store.py
@@ -7,8 +7,12 @@ from   apsis.runs import Instance, Run, RunStore
 
 class MockRunDb:
 
+    def __init__(self, runs=()):
+        self.__runs = runs
+
+
     def query(self, min_timestamp=None):
-        return iter(())
+        return iter(self.__runs)
 
 
     def upsert(self, run):
@@ -30,8 +34,8 @@ class MockRunIdDb:
 
 class MockDb:
 
-    def __init__(self):
-        self.run_db = MockRunDb()
+    def __init__(self, runs=()):
+        self.run_db = MockRunDb(runs)
         self.next_run_id_db = MockRunIdDb()
 
 
@@ -99,5 +103,66 @@ def test_runs_by_job():
         for j in job_ids 
     ))
     assert r == set(run_ids)
+
+
+def test_run_store_populate():
+    """
+    Tests a RunStore populated from existing runs.
+
+    Existing runs come from a mocked run DB.
+    """
+    rnd = random.Random(0)
+    n = 10000
+
+    job_ids = [ f"job{i:02d}" for i in range(100) ]
+    rnd.shuffle(job_ids)
+
+    # Since we're not calling run_store.add(), we have to assign run IDs.
+    run_ids = MockRunIdDb()
+    def make_run():
+        run = Run(Instance(rnd.choice(job_ids), {}), expected=True)
+        run.run_id = run_ids.get_next_run_id()
+        return run
+
+    runs = [ make_run() for _ in range(n) ]
+    run_ids = { r.run_id for r in runs }
+    assert len(run_ids) == len(runs)
+
+    # Create a random run store and add a bunch of runs.
+    run_store = RunStore(MockDb(runs), min_timestamp=ora.now())
+
+    # Query each job by run ID.
+    for run in runs:
+        assert list(run_store.query(run_ids=run.run_id)[1]) == [run]
+
+    # Query jobs by job ID.
+    for job_id in job_ids:
+        q = set(run_store.query(job_id=job_id)[1])
+        assert q == { r for r in runs if r.inst.job_id == job_id }
+
+    # Now remove some runs.
+    for run in rnd.sample(runs, len(runs) // 4):
+        run_store.remove(run)
+        runs.remove(run)
+
+    # Query each job by run ID.
+    for run in runs:
+        assert list(run_store.query(run_ids=run.run_id)[1]) == [run]
+
+    # Query jobs by job ID.
+    for job_id in job_ids:
+        q = set(run_store.query(job_id=job_id)[1])
+        assert q == { r for r in runs if r.inst.job_id == job_id }
+
+    # Now remove the rest of the runs.
+    for run in runs:
+        run_store.remove(run)
+
+    # All queries should be empty.
+    assert len(list(run_store.query())) == 0
+    for run_id in run_ids:
+        assert len(list(run_store.query(run_ids=run_id))) == 0
+    for job_id in job_ids:
+        assert len(list(run_store.query(job_id=job_id))) == 0
 
 
