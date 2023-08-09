4524021ba8b9cafe3aa342c12ca4d797f849e392
Alex Samuel <alex@alexsamuel.net>
2023-08-09 09:56:09 -0400

    Improve test.

--- test/int/action/test_schedule_action.py
+++ test/int/action/test_schedule_action.py
@@ -27,15 +27,20 @@ def test_schedule_action(inst):
 
     inst.wait_run(r1)
 
+    # first(label=foo) should have scheduled second(label=on-success).
     run, = tuple(client.get_runs(job_id="second").values())
     assert run["args"] == {"label": "on-success"}
 
+    # first(label=foo) should not have scheduled third.
     runs = tuple(client.get_runs(job_id="third").values())
     assert len(runs) == 0
 
+    # second(label=on-success) should have scheduled fourth().
     runs = tuple(client.get_runs(job_id="fourth").values())
     assert len(runs) == 1
 
+    # first(label=foo) should have scheduled fifth(label=foo) with automatic
+    # label.
     run, = tuple(client.get_runs(job_id="fifth").values())
     assert run["args"] == {"label": "foo"}
 
