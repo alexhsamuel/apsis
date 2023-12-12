556819d5eae709096af0e14d7c170332a23cf174
Alex Samuel <alex@alexsamuel.net>
2023-12-12 15:15:58 -0500

    Reconnect int test.

---
+++ test/int/basic/test_reconnect.py
@@ -0,0 +1,38 @@
+from   contextlib import closing
+import logging
+from   pathlib import Path
+
+from   instance import ApsisInstance
+
+#-------------------------------------------------------------------------------
+
+def test_reconnect(tmpdir):
+    job_dir = Path(__file__).parent / "jobs"
+    with closing(ApsisInstance(job_dir=job_dir)) as inst:
+        inst.create_db()
+        inst.write_cfg()
+        inst.start_serve()
+        inst.wait_for_serve()
+
+        run_ids = [
+            inst.client.schedule("sleep", {"time": 1})["run_id"]
+            for _ in range(8)
+        ]
+        for run_id in run_ids:
+            res = inst.wait_run(run_id, wait_states=("new", "starting"))
+            assert res["state"] == "running"
+
+        logging.info("restarting")
+        inst.stop_serve()
+        inst.start_serve()
+        inst.wait_for_serve()
+        logging.info("restarted")
+
+        for run_id in run_ids:
+            res = inst.client.get_run(run_id)
+            assert res["state"] == "running"
+        for run_id in run_ids:
+            res = inst.wait_run(run_id)
+            assert res["state"] == "success"
+
+
