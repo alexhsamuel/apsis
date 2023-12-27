f2651f3199a1cde90088b5c2007d64b9b4708991
Alex Samuel <alex@alexsamuel.net>
2023-12-27 16:23:29 -0500

    Integration tests for dependency with exist.

--- test/int/cond/test_dep.py
+++ test/int/cond/test_dep.py
@@ -129,3 +129,106 @@ def test_many_deps(inst, direction):
     assert res["state"] == "success"
 
 
+def test_exist(inst):
+    client = inst.client
+    date = "2023-12-27"
+
+    # Schedule the dependent without any dependency; this results in error
+    # because depedency(flavor=vanilla) and dependency(flavor=chocolate) must
+    # exist.
+    res = client.schedule("exist", {"date": date, "color": "red"})
+    run_id = res["run_id"]
+    res = inst.wait_run(run_id)
+    assert res["state"] == "error"
+
+    # Schedule the first dependency.
+    res = client.schedule("dependency", {"date": date, "flavor": "chocolate"})
+    inst.wait_run(res["run_id"])
+
+    # One dependency isn't enough.
+    res = client.schedule("exist", {"date": date, "color": "red"})
+    run_id = res["run_id"]
+    res = inst.wait_run(run_id)
+    assert res["state"] == "error"
+
+    # Schedule the second dependency.
+    res = client.schedule("dependency", {"date": date, "flavor": "vanilla"})
+    inst.wait_run(res["run_id"])
+
+    # Both dependencies should exist, so we can proceed.
+    res = client.schedule("exist", {"date": date, "color": "red"})
+    run_id = res["run_id"]
+    res = inst.wait_run(run_id)
+    assert res["state"] == "success"
+
+
+def test_exist_slow(inst):
+    client = inst.client
+    date = "2023-12-27"
+
+    # Schedule the dependent without any dependency; this results in error
+    # because depedency(flavor=vanilla) and dependency(flavor=chocolate) must
+    # exist.
+    res = client.schedule("exist slow", {"date": date, "color": "red"})
+    run_id = res["run_id"]
+    res = inst.wait_run(run_id)
+    assert res["state"] == "error"
+
+    # Schedule the first dependency.
+    res = client.schedule("slow", {"date": date, "flavor": "chocolate"})
+
+    # Try the dependent again.  The dependency, slow(flavor=chocolate), isn't
+    # done yet, but that's OK.
+    res = client.schedule("exist slow", {"date": date, "color": "red"})
+    run_id = res["run_id"]
+    res = inst.wait_run(run_id)
+    assert res["state"] == "error"
+
+    # Schedule the second dependency.
+    res = client.schedule("slow", {"date": date, "flavor": "vanilla"})
+
+    # Both dependencies exist, whether or not they are successful, so OK.
+    res = client.schedule("exist slow", {"date": date, "color": "red"})
+    run_id = res["run_id"]
+    res = inst.wait_run(run_id)
+    assert res["state"] == "success"
+
+
+def test_exist_fail(inst):
+    client = inst.client
+    date = "2023-12-27"
+
+    # Schedule the dependent without any dependency; this results in error
+    # because depedency(flavor=vanilla) and dependency(flavor=chocolate) must
+    # exist.
+    res = client.schedule("exist fail", {"date": date, "color": "red"})
+    run_id = res["run_id"]
+    res = inst.wait_run(run_id)
+    assert res["state"] == "error"
+
+    # Schedule the dependency.
+    dep_run_id = client.schedule("fail", {"date": date})["run_id"]
+
+    # Now schedule the dependent should work.  The dependency is running, so
+    # this should enter waiting.
+    res = client.schedule("exist fail", {"date": date, "color": "red"})
+    run_id = res["run_id"]
+    time.sleep(0.1)
+    res = client.get_run(run_id)
+    assert res["state"] == "waiting"
+
+    # Wait for the dependency to fail.
+    res = inst.wait_run(dep_run_id)
+    assert res["state"] == "failure"
+
+    # The dependent should error too.
+    res = inst.wait_run(run_id)
+    assert res["state"] == "error"
+
+    # Now the dependent should no longer work; the dependency is running.
+    res = client.schedule("exist fail", {"date": date, "color": "red"})
+    run_id = res["run_id"]
+    res = inst.wait_run(run_id)
+    assert res["state"] == "error"
+
+
