9e51d27d3e7f640d1f18a811ed7523819cb54685
Alex Samuel <alex@alexsamuel.net>
2024-02-01 15:06:33 -0500

    Check for agent cleanup after reconnect.

--- test/int/procstar/test_program.py
+++ test/int/procstar/test_program.py
@@ -7,10 +7,7 @@ JOB_DIR = Path(__file__).parent / "jobs"
 #-------------------------------------------------------------------------------
 
 def test_program():
-    with (
-            ApsisService(job_dir=JOB_DIR) as svc,
-            svc.agent(serve=True) as agent,
-    ):
+    with ApsisService(job_dir=JOB_DIR) as svc, svc.agent(serve=True) as agent:
         assert len(agent.client.get_procs()) == 0
         run_id = svc.client.schedule("sleep", {"time": 1})["run_id"]
         res = svc.wait_run(run_id, timeout=5)
@@ -22,7 +19,7 @@ def test_reconnect():
     """
     Tests reconnecting to a running run after Apsis restart.
     """
-    with ApsisService(job_dir=JOB_DIR) as svc, svc.agent():
+    with ApsisService(job_dir=JOB_DIR) as svc, svc.agent(serve=True) as agent:
         run_id = svc.client.schedule("sleep", {"time": 1})["run_id"]
         # Wait for the run to start; we can't reconnect to starting runs.
         res = svc.wait_run(run_id, wait_states=("starting", ))
@@ -32,6 +29,7 @@ def test_reconnect():
 
         res = svc.wait_run(run_id, timeout=5)
         assert res["state"] == "success"
+        assert len(agent.client.get_procs()) == 0
 
 
 def test_reconnect_many(num=256):
@@ -57,7 +55,6 @@ def test_reconnect_many(num=256):
 
 
 # FIXME:
-# - check that procstar has no undeleted runs at shutdown
 # - signal tests
 # - procstar connection timeout and reconnect (SIGHUP)
 
