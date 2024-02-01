5e5092b93a8f93e3a52099ec4d18e3eb9396eeaf
Alex Samuel <alex@alexsamuel.net>
2024-02-01 15:05:15 -0500

    Make sure the agent is empty.

--- test/int/procstar/test_program.py
+++ test/int/procstar/test_program.py
@@ -7,10 +7,15 @@ JOB_DIR = Path(__file__).parent / "jobs"
 #-------------------------------------------------------------------------------
 
 def test_program():
-    with ApsisService(job_dir=JOB_DIR) as svc, svc.agent():
+    with (
+            ApsisService(job_dir=JOB_DIR) as svc,
+            svc.agent(serve=True) as agent,
+    ):
+        assert len(agent.client.get_procs()) == 0
         run_id = svc.client.schedule("sleep", {"time": 1})["run_id"]
         res = svc.wait_run(run_id, timeout=5)
         assert res["state"] == "success"
+        assert len(agent.client.get_procs()) == 0
 
 
 def test_reconnect():
