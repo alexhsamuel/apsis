a1235cc044397909ec8899e5f0fb3d1074edc350
Alex Samuel <alex@alexsamuel.net>
2024-02-01 16:50:57 -0500

    Check empty agent.

--- test/int/procstar/test_program.py
+++ test/int/procstar/test_program.py
@@ -36,7 +36,7 @@ def test_reconnect_many(num=256):
     """
     Tests reconnecting to many running runs after Apsis restart.
     """
-    with ApsisService(job_dir=JOB_DIR) as svc, svc.agent():
+    with ApsisService(job_dir=JOB_DIR) as svc, svc.agent(serve=True) as agent:
         run_ids = [
             svc.client.schedule("sleep", {"time": 1})["run_id"]
             for _ in range(num)
@@ -52,6 +52,8 @@ def test_reconnect_many(num=256):
         for run_id in run_ids:
             res = svc.wait_run(run_id, timeout=5)
             assert res["state"] == "success"
+
+        assert len(agent.client.get_procs()) == 0
 
 
 # FIXME:
