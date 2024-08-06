03dbf0faa6bb1f8569be8638bf8080817ea93db1
Alex Samuel <alex@alexsamuel.net>
2024-08-06 16:08:01 -0400

    Publisher unit test.

--- test/unit/test_asyn.py
+++ test/unit/test_asyn.py
@@ -34,3 +34,68 @@ async def test_communicate_timeout():
         assert False, "should have timed out"
 
 
+@pytest.mark.asyncio
+async def test_publisher():
+    pub = apsis.lib.asyn.Publisher()
+
+    with (
+            pub.subscription() as sub_all,
+            pub.subscription(predicate=lambda n: n % 2 == 0) as sub_even,
+            pub.subscription(predicate=lambda n: n % 10 == 0) as sub_tens,
+    ):
+        assert pub.num_subs == 3
+
+        fut = asyncio.ensure_future(anext(sub_all))
+        assert not fut.done()
+        pub.publish(0)
+        assert await fut == 0
+        assert await anext(sub_even) == 0
+        assert await anext(sub_tens) == 0
+
+        for i in range(1, 11):
+            pub.publish(i)
+        assert [ await anext(sub_even) for _ in range(5) ] == [2, 4, 6, 8, 10]
+        fut = asyncio.ensure_future(anext(sub_tens))
+        assert await fut == 10
+        fut = asyncio.ensure_future(anext(sub_tens))
+        assert not fut.done()
+        assert [ await anext(sub_all) for _ in range(10) ] == list(range(1, 11))
+        pub.publish(20)
+        assert await fut == 20
+        assert await anext(sub_all) == 20
+        assert await anext(sub_even) == 20
+
+        pub.publish(30)
+        pub.publish(31)
+        pub.close()
+
+        # Can't publish after close.
+        with pytest.raises(RuntimeError):
+            pub.publish(32)
+
+        # Closing multiple times is OK.
+        pub.close()
+
+        assert await anext(sub_tens) == 30
+        with pytest.raises(StopAsyncIteration):
+            await anext(sub_tens)
+        assert await anext(sub_even) == 30
+        with pytest.raises(StopAsyncIteration):
+            await anext(sub_even)
+        assert await anext(sub_all) == 30
+        assert await anext(sub_all) == 31
+        with pytest.raises(StopAsyncIteration):
+            await anext(sub_all)
+        with pytest.raises(StopAsyncIteration):
+            await anext(sub_all)
+
+    assert pub.num_subs == 0
+
+    # Subscription after close should immediately be closed.
+    with pub.subscription() as sub_late:
+        assert pub.num_subs == 1
+        with pytest.raises(StopAsyncIteration):
+            await anext(sub_late)
+
+
+
