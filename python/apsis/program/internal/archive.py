ca0ce12672428f1da9f375ab18a63c7fff15295d
Alex Samuel <alex@alexsamuel.net>
2024-12-04 22:58:20 -0500

    Archive chunk size.

--- python/apsis/program/internal/archive.py
+++ python/apsis/program/internal/archive.py
@@ -23,7 +23,7 @@ class ArchiveProgram(_InternalProgram):
     skipped for archiving.
     """
 
-    def __init__(self, *, age, path, count):
+    def __init__(self, *, age, path, count, chunk_size=None):
         """
         If this archive file doesn't exist, it is created automatically on
         first use; the contianing directory must exist.
@@ -35,10 +35,13 @@ class ArchiveProgram(_InternalProgram):
           Apsis database file.
         :param count:
           Maximum number of runs to archive per run of this program.
+        :param chunk_size:
+          Number of runs to archive in one chunk.  Each chunk is blocking.
         """
-        self.__age = age
-        self.__path = path
-        self.__count = count
+        self.__age          = age
+        self.__path         = path
+        self.__count        = count
+        self.__chunk_size   = chunk_size
 
 
     def __str__(self):
@@ -46,27 +49,35 @@ class ArchiveProgram(_InternalProgram):
 
 
     def bind(self, args):
-        age = parse_duration(template_expand(self.__age, args))
-        path = template_expand(self.__path, args)
-        count = int(template_expand(self.__count, args))
-        return type(self)(age=age, path=path, count=count)
+        return type(self)(
+            age         = parse_duration(template_expand(self.__age, args)),
+            path        = template_expand(self.__path, args),
+            count       = int(template_expand(self.__count, args)),
+            chunk_size  = None if self.__chunk_size is None
+                          else int(template_expand(self.__chunk_size, args)),
+        )
 
 
     @classmethod
     def from_jso(cls, jso):
         with check_schema(jso) as pop:
-            age = pop("age")
-            path = pop("path", str)
-            count = pop("count", int)
-        return cls(age=age, path=path, count=count)
+            age         = pop("age")
+            path        = pop("path", str)
+            count       = pop("count", int)
+            chunk_size  = pop("chunk_size", int, None)
+        return cls(age=age, path=path, count=count, chunk_size=chunk_size)
 
 
     def to_jso(self):
         return {
             **super().to_jso(),
-            "age": self.__age,
-            "path": self.__path,
-            "count": self.__count,
+            "age"   : self.__age,
+            "path"  : self.__path,
+            "count" : self.__count,
+            **(
+                {} if self.__chunk_size is None
+                else {"chunk_size": self.__chunk_size}
+            ),
         }
 
 
@@ -78,28 +89,43 @@ class ArchiveProgram(_InternalProgram):
         # FIXME: Private attributes.
         db = apsis._Apsis__db
 
-        run_ids = db.get_archive_run_ids(
-            before  =ora.now() - self.__age,
-            count   =self.__count,
-        )
+        if not (self.__chunk_size is None or 0 < self.__chunk_size):
+            raise ValueError("nonpositive chunk size")
 
-        # Make sure all runs are retired; else skip them.
-        run_ids = [ r for r in run_ids if apsis.run_store.retire(r) ]
+        row_counts = {}
+        meta = {
+            "run count" : 0,
+            "run_ids"   : [],
+            "row counts": row_counts
+        }
 
-        if len(run_ids) > 0:
-            # Archive these runs.
-            row_counts = db.archive(self.__path, run_ids)
-            # Also vacuum to free space.
-            db.vacuum()
+        count = self.__count
+        while count > 0:
+            chunk = (
+                count if self.__chunk_size is None
+                else min(count, self.__chunk_size)
+            )
+            run_ids = db.get_archive_run_ids(
+                before  =ora.now() - self.__age,
+                count   =chunk,
+            )
+            count -= chunk
 
-        else:
-            row_counts = {}
+            # Make sure all runs are retired; else skip them.
+            run_ids = [ r for r in run_ids if apsis.run_store.retire(r) ]
 
-        return ProgramSuccess(meta={
-            "run count" : len(run_ids),
-            "run_ids"   : run_ids,
-            "row counts": row_counts,
-        })
+            if len(run_ids) > 0:
+                # Archive these runs.
+                chunk_row_counts = db.archive(self.__path, run_ids)
+                # Accumulate metadata.
+                meta["run count"] += len(run_ids)
+                meta["run_ids"].extend(run_ids)
+                for key, value in chunk_row_counts.items():
+                    row_counts[key] = row_counts.get(key, 0) + value
+                # Also vacuum to free space.
+                db.vacuum()
+
+        return ProgramSuccess(meta=meta)
 
 
     def reconnect(self, run_id, run_state, apsis):
