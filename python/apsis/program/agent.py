b9cb61747366c0159fd9a4c08883c06d5300a5dc
Alex Samuel <alex@alexsamuel.net>
2025-01-05 00:10:06 -0500

    Fix running program for classic agent.

--- python/apsis/program/agent.py
+++ python/apsis/program/agent.py
@@ -72,154 +72,12 @@ class AgentProgram(Program):
         return cls(argv, host=host, user=user, timeout=timeout)
 
 
-    async def start(self, run_id, cfg):
-        host = self.get_host(cfg)
-        argv = self.argv
-
-        loc = "" if host is None else " on " + host
-        cmd = join_args(argv)
-        log.debug(f"starting program{loc}: {cmd}")
-
-        env = {
-            "inherit": True,
-            "vars": {
-                "APSIS_RUN_ID": run_id,
-                # FIXME: Other things?
-            },
-        }
-
-        meta = {
-            "apsis_hostname"  : socket.gethostname(),
-            "apsis_username"  : get_username(),
-        }
-
-        try:
-            agent = self.__get_agent(host)
-            proc = await agent.start_process(argv, env=env, restart=True)
-
-        except Exception as exc:
-            log.error("failed to start process", exc_info=True)
-            output = traceback.format_exc().encode()
-            # FIXME: Use a different "traceback" output, once the UI can
-            # understand it.
-            raise ProgramError(
-                message=str(exc), outputs=program_outputs(output))
-
-        state = proc["state"]
-        if state == "run":
-            log.debug(f"program running: {run_id} as {proc['proc_id']}")
-
-            run_state = {
-                "host"          : host,
-                "proc_id"       : proc["proc_id"],
-                "pid"           : proc["pid"],
-            }
-            meta.update(run_state)
-            # FIXME: Propagate times from agent.
-            # FIXME: Do this asynchronously from the agent instead.
-            done = self.wait(run_id, run_state)
-            run_state["start"] = str(ora.now())
-            return ProgramRunning(run_state, meta=meta), done
-
-        elif state == "err":
-            message = proc.get("exception", "program error")
-            log.info(f"program error: {run_id}: {message}")
-            # Clean up the process from the agent.
-            await agent.del_process(proc["proc_id"])
-
-            raise ProgramError(message)
-
-        else:
-            assert False, f"unknown state: {state}"
+    def run(self, run_id, cfg):
+        return RunningAgentProgram(run_id, self, cfg)
 
 
-    async def wait(self, run_id, run_state):
-        host = run_state["host"]
-        proc_id = run_state["proc_id"]
-        agent = self.__get_agent(host)
-        if self.timeout is not None:
-            try:
-                start = ora.Time(run_state["start"])
-            except KeyError:
-                # Backward compatibility: no start in run state.
-                # FIXME: Clean this up after transition.
-                start = ora.now()
-
-        explanation = ""
-
-        # FIXME: This is so embarrassing.
-        POLL_INTERVAL = 1
-
-        if HTTP_IMPL == "httpx":
-            TIMEOUT = 60
-            client_ctx = httpx.AsyncClient(
-                verify=False,
-                timeout=httpx.Timeout(TIMEOUT),
-                limits=httpx.Limits(
-                    max_keepalive_connections=1,
-                    keepalive_expiry=TIMEOUT,
-                ),
-            )
-        else:
-            @contextlib.asynccontextmanager
-            async def null_ctx():
-                yield None
-
-            client_ctx = null_ctx()
-
-        async with client_ctx as client:
-            while True:
-                if self.timeout is not None:
-                    elapsed = ora.now() - start
-                    if self.timeout.duration < elapsed:
-                        msg = f"timeout after {elapsed:.0f} s"
-                        log.info(f"{run_id}: {msg}")
-                        explanation = f" ({msg})"
-                        # FIXME: Note timeout in run log.
-                        await self.signal(run_id, run_state, self.timeout.signal)
-                        await asyncio.sleep(POLL_INTERVAL)
-
-                log.debug(f"polling proc: {run_id}: {proc_id} @ {host}")
-                try:
-                    proc = await agent.get_process(proc_id, restart=True, client=client)
-                except NoSuchProcessError:
-                    # Agent doesn't know about this process anymore.
-                    raise ProgramError(f"program lost: {run_id}")
-                if proc["state"] == "run":
-                    await asyncio.sleep(POLL_INTERVAL)
-                else:
-                    break
-
-            status = proc["status"]
-            output, length, compression = await agent.get_process_output(proc_id, client=client)
-
-            if compression is None and len(output) > 16384:
-                # Compress the output.
-                try:
-                    output, compression = await compress_async(output, "br"), "br"
-                except RuntimeError as exc:
-                    log.error(f"{exc}; not compressing")
-
-            outputs = program_outputs(
-                output, length=length, compression=compression)
-            log.debug(f"got output: {length} bytes, {compression or 'uncompressed'}")
-
-            try:
-                if status == 0:
-                    return ProgramSuccess(meta=proc, outputs=outputs)
-
-                else:
-                    message = f"program failed: status {status}{explanation}"
-                    raise ProgramFailure(message, meta=proc, outputs=outputs)
-
-            finally:
-                # Clean up the process from the agent.
-                await agent.del_process(proc_id, client=client)
-
-
-    def reconnect(self, run_id, run_state):
-        log.debug(f"reconnect: {run_id}")
-        return asyncio.ensure_future(self.wait(run_id, run_state))
+    def connect(self, run_id, run_state, cfg):
+        return RunningAgentProgram(run_id, self, cfg, run_state)
 
 
 
@@ -234,9 +92,9 @@ class AgentShellProgram(AgentProgram):
 
     def bind(self, args):
         command = template_expand(self.command, args)
-        host    = or_none(template_expand)(self._AgentProgram__host, args)
-        user    = or_none(template_expand)(self._AgentProgram__user, args)
-        timeout = self._AgentProgram__timeout
+        host    = or_none(template_expand)(self.host, args)
+        user    = or_none(template_expand)(self.user, args)
+        timeout = self.timeout
         timeout = None if timeout is None else timeout.bind(args)
         return type(self)(command, host=host, user=user, timeout=timeout)
 
@@ -277,12 +135,13 @@ class RunningAgentProgram(RunningProgram):
 
     def __get_agent(self, host):
         host = None if host is None else socket.getfqdn(host)
-        return _get_agent(host, self.user)
+        return _get_agent(host, self.program.user)
 
 
     @memo.property
-    def updates(self):
+    async def updates(self):
         if self.run_state is None:
+            # Start the proc.
             host = expand_host(self.program.host, self.cfg)
             argv = self.program.argv
 
@@ -333,9 +192,11 @@ class RunningAgentProgram(RunningProgram):
                 assert False, f"unknown state: {state}"
 
         else:
-            proc = ...
+            # Poll an existing proc.
             proc_id = self.run_state["proc_id"]
-            start = ora.Time(self.run_state["start"])
+            host    = self.run_state["host"]
+            start   = ora.Time(self.run_state["start"])
+            agent   = self.__get_agent(host)
 
         #------------
 
@@ -356,14 +217,14 @@ class RunningAgentProgram(RunningProgram):
 
         async with client_ctx as client:
             while True:
-                if self.timeout is not None:
+                if self.program.timeout is not None:
                     elapsed = ora.now() - start
-                    if self.timeout.duration < elapsed:
+                    if self.program.timeout.duration < elapsed:
                         msg = f"timeout after {elapsed:.0f} s"
                         log.info(f"{self.run_id}: {msg}")
                         explanation = f" ({msg})"
                         # FIXME: Note timeout in run log.
-                        await self.signal(self.run_id, self.run_state, self.timeout.signal)
+                        await self.signal(self.program.timeout.signal)
                         await asyncio.sleep(POLL_INTERVAL)
 
                 log.debug(f"polling proc: {self.run_id}: {proc_id} @ {host}")
@@ -393,15 +254,16 @@ class RunningAgentProgram(RunningProgram):
 
             try:
                 if status == 0:
-                    return ProgramSuccess(meta=proc, outputs=outputs)
+                    yield ProgramSuccess(meta=proc, outputs=outputs)
 
                 else:
                     message = f"program failed: status {status}{explanation}"
-                    raise ProgramFailure(message, meta=proc, outputs=outputs)
+                    yield ProgramFailure(message, meta=proc, outputs=outputs)
 
             finally:
                 # Clean up the process from the agent.
                 await agent.del_process(proc_id, client=client)
+
 
     async def signal(self, signal):
         """
@@ -415,5 +277,6 @@ class RunningAgentProgram(RunningProgram):
         proc_id = self.run_state["proc_id"]
         agent = self.__get_agent(self.run_state["host"])
         await agent.signal(proc_id, signal)
+
 
 
