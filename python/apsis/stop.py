3400fd6d1d8c57b0614a15f47069a495aea5507f
Alex Samuel <alex@alexsamuel.net>
2025-01-05 00:16:40 -0500

    Clean up no longer used.

--- python/apsis/stop.py
+++ python/apsis/stop.py
@@ -1,72 +1,8 @@
-import asyncio
 import ora
-from   signal import Signals
 
-from   apsis.lib.json import TypedJso, to_array, check_schema
+from   apsis.lib.json import TypedJso, check_schema
 from   apsis.lib.parse import parse_duration
 from   apsis.lib.py import format_ctor
-from   apsis.lib.sys import to_signal
-from   apsis.states import State, to_state, reachable
-
-#-------------------------------------------------------------------------------
-
-class StopMethod(TypedJso):
-
-    TYPE_NAMES = TypedJso.TypeNames()
-
-    async def __call__(self, apsis, run):
-        raise NotImplementedError("StopMethod.__call__")
-
-
-
-class StopSignalMethod:
-    """
-    Stops a program by sending a signal.
-
-    Sends `signal`, waits `timeout` seconds, then sends SIGKILL.
-    """
-
-    def __init__(self, signal=Signals.SIGTERM, timeout=60):
-        self.signal     = to_signal(signal)
-        self.timeout    = float(timeout)
-        assert 0 <= self.timeout
-
-
-    def __eq__(self, other):
-        return other.signal == self.signal and other.timeout == self.timeout
-
-
-    def __repr__(self):
-        return format_ctor(self, signal=self.signal, timeout=self.timeout)
-
-
-    def __str__(self):
-        return f"signal {self.signal.name}"
-
-
-    def to_jso(self):
-        return {
-            **super().to_jso(),
-            "signal"    : self.signal.name,
-            "timeout"   : self.timeout,
-        }
-
-
-    @classmethod
-    def from_jso(cls, jso):
-        with check_schema(jso) as pop:
-            signal      = pop("signal", to_signal, Signal.SIGTERM),
-            timeout     = pop("timeout", float, 60),
-            return cls(signal, timeout)
-
-
-    async def __call__(self, apsis, run):
-        await apsis.send_signal(run, self.signal)
-        await asyncio.sleep(self.timeout)
-        if not run.state.finished:
-            await asyncio.send_signal(run, Signal.SIGKILL)
-
-
 
 #-------------------------------------------------------------------------------
 
